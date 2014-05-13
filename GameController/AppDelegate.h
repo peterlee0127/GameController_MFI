fe2f5e76aeb52b6dfaf385e14ae658c3a45857ab
Peterlee <peterlee0127@gmail.com>
2014-05-13 12:42:13 +0800

    Initial Commit

---
+++ GameController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GameController
+//
+//  Created by Peterlee on 5/13/14.
+//  Copyright (c) 2014 Peterlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GameController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GameController
+//
+//  Created by Peterlee on 5/13/14.
+//  Copyright (c) 2014 Peterlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
